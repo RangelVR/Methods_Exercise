7ed982785cd9c060441d354da8deb9970e4a08fa
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-17 23:14:46 +0300

    Add files via upload

---
+++ 01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMinValue(num1, num2, num3));
+        }
+
+        static int GetMinValue(int num1, int num2, int num3)
+        {
+            int[] allNums = { num1, num2, num3 };
+            int minValue = allNums[0];
+            foreach (var num in allNums)
+            {
+                if (num < minValue)
+                {
+                    minValue = num;
+                }
+            }
+            return minValue;
+        }
+        //static void Main(string[] args)
+        //{
+        //    int[] nums = new int [3];
+        //    int theSmallNum = 0;
+
+        //    GetAllNums(nums);
+        //    Console.WriteLine(PrintTheSmallestNum(nums, theSmallNum));
+        //}
+
+        //static void GetAllNums(int[] allNums) 
+        //{
+        //    for (int i = 0; i < 3; i++)
+        //    {
+        //        int n = int.Parse(Console.ReadLine());
+        //        allNums[i] = n;
+        //    }
+        //}
+        //static int PrintTheSmallestNum(int[] nums, int theSmallNum) 
+        //{
+        //    theSmallNum = nums[0];
+        //    for (int i = 0; i < 3; i++)
+        //    {
+        //        if (nums[i] < theSmallNum)
+        //        {
+        //            theSmallNum = nums[i];
+        //        }
+        //    }
+        //    return theSmallNum;
+        //}
+
+
+    }
+}
