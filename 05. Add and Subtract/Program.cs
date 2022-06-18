08d8be5c41d741a4c680791e521743637221c1da
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-18 15:52:30 +0300

    Update Program.cs

--- 05. Add and Subtract/Program.cs
+++ 05. Add and Subtract/Program.cs
@@ -10,7 +10,6 @@ namespace _05._Add_and_Subtract
             int n2 = int.Parse(Console.ReadLine());
             int n3 = int.Parse(Console.ReadLine());
             Console.WriteLine(PrintResult(n1, n2, n3));
-
         }
 
         static int PrintResult(int n1, int n2, int n3)
