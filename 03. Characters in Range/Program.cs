5e68dcb548f40ed9fc118dbde38f1be1170e86c4
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-18 13:35:35 +0300

    Add files via upload

---
+++ 03. Characters in Range/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char begin = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            PrintCharsBetwin(begin, end);
+        }
+
+        static void PrintCharsBetwin(char start, char stop)
+        {
+            if (start > stop)
+            {
+                for (char i = (char)(stop + 1); i < start; i++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            else
+            {
+                for (char i = (char)(start + 1); i < stop; i++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    char startChar = char.Parse(Console.ReadLine());
+        //    char stopChar = char.Parse(Console.ReadLine());
+
+        //    PrintTheCharsBetweenTwoChars(startChar, stopChar);
+        //}
+
+        //static void PrintTheCharsBetweenTwoChars(char firstCharacter, char secondCharacter)
+        //{
+
+        //    int startCh = Math.Min(firstCharacter, secondCharacter);
+        //    int stopCh = Math.Max(firstCharacter, secondCharacter);
+
+        //    for (int i = ++startCh; i < stopCh; i++)
+        //    {
+        //        Console.Write((char)i + " ");
+        //    }
+
+        //}
+    }
+}
