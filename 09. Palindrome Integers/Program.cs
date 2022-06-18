0663a441aebed4f05979e4df3333b71b49930ed7
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-18 21:36:01 +0300

    Add files via upload

---
+++ 09. Palindrome Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n;
+            while ((n = Console.ReadLine()) != "END")
+            {
+                Console.WriteLine(PrintTrueOrFalse(n));
+            }
+        }
+
+        static bool PrintTrueOrFalse(string n)
+        {
+            string reversedNum = string.Empty;
+            bool result = false;
+
+            for (int i = n.Length - 1; i >= 0; i--)
+            {
+                reversedNum += n[i];
+            }
+            if (n == reversedNum)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
