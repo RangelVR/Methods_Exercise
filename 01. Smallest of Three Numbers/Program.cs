6384de914a69624750fa07e32c981d01cda4d566
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-18 13:41:55 +0300

    Update Program.cs

--- 01. Smallest of Three Numbers/Program.cs
+++ 01. Smallest of Three Numbers/Program.cs
@@ -1,34 +1,59 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace _01._Smallest_of_Three_Numbers
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int num1 = int.Parse(Console.ReadLine());
-            int num2 = int.Parse(Console.ReadLine());
-            int num3 = int.Parse(Console.ReadLine());
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(GetMinValue(num1, num2, num3));
+            Console.WriteLine(GetMinValue(n1, n2, n3));
         }
 
-        static int GetMinValue(int num1, int num2, int num3)
+        static int GetMinValue(int n1, int n2, int n3)
         {
-            int[] allNums = { num1, num2, num3 };
-            int minValue = allNums[0];
-            foreach (var num in allNums)
+            int minValue = n1;
+            if (n2 < minValue)
             {
-                if (num < minValue)
-                {
-                    minValue = num;
-                }
+                minValue = n2;
+            }
+            if (n3 < minValue)
+            {
+                minValue = n3;
             }
             return minValue;
         }
-        -----------------------------------------------------------------------------------------------
-             //static void Main(string[] args)
+------------------------------------------------------------------------------------------
+        //static void Main(string[] args)
+        //{
+        //    int num1 = int.Parse(Console.ReadLine());
+        //    int num2 = int.Parse(Console.ReadLine());
+        //    int num3 = int.Parse(Console.ReadLine());
+
+        //    Console.WriteLine(GetMinValue(num1, num2, num3));
+        //}
+
+        //static int GetMinValue(int num1, int num2, int num3)
+        //{
+        //    int[] allNums = { num1, num2, num3 };
+        //    int minValue = allNums[0];
+        //    foreach (var num in allNums)
+        //    {
+        //        if (num < minValue)
+        //        {
+        //            minValue = num;
+        //        }
+        //    }
+        //    return minValue;
+        //}
+-------------------------------------------------------------------------------------------------
+
+        //static void Main(string[] args)
         //{
         //    int[] nums = new int [3];
         //    int theSmallNum = 0;
@@ -57,29 +82,7 @@ namespace _01._Smallest_of_Three_Numbers
         //    }
         //    return theSmallNum;
         //}
-        ------------------------------------------------------------------------------------------------
-             //static void Main(string[] args)
-        //{
-        //    int n1 = int.Parse(Console.ReadLine());
-        //    int n2 = int.Parse(Console.ReadLine());
-        //    int n3 = int.Parse(Console.ReadLine());
 
-        //    Console.WriteLine(GetMinValue(n1, n2, n3));
-        //}
-
-        //static int GetMinValue(int n1, int n2, int n3) 
-        //{
-        //    int minValue = n1;
-        //    if (n2 < minValue)
-        //    {
-        //        minValue = n2;
-        //    }
-        //    if (n3 < minValue)
-        //    {
-        //        minValue = n3;
-        //    }
-        //    return minValue;
-        //}
 
 
     }
