389e33005dca413ebe6705bcf3c585de2a2eec74
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-18 12:33:11 +0300

    Add files via upload

---
+++ 02. Vowels Count/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            GetCountVowels(input);
+        }
+
+        static void GetCountVowels(string input) 
+        {
+            int countVowels = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        countVowels++;
+                        break;
+                }
+            }
+            Console.WriteLine(countVowels);
+        }
+    }
+}
