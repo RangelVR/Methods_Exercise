31035a81e0388d769bc8f38375550b39a5171a14
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-19 13:07:45 +0300

    Add files via upload

---
+++ 10. Top Number/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._Top_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            PrintTopNum(num);
+        }
+
+        static void PrintTopNum(int num)
+        {
+            for (int i = 17; i <= num; i++)
+            {
+                if (GetSumDigits(i))
+                {
+                    if (IsOddNumber(i))
+                    {
+                        Console.WriteLine(i);
+                    }
+                  
+                }
+            }
+        }
+
+        static bool GetSumDigits(int num)
+        {
+            int sumDigits = 0;
+            while (num != 0)
+            {
+                sumDigits += num % 10;
+                num /= 10;
+            }
+            if (sumDigits % 8 == 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        static bool IsOddNumber(int i)
+        {
+            for (int k = i; k <= i; k++)
+            {
+                int oddNum = 0;
+                while (i != 0)
+                {
+                    oddNum += i % 10;
+                    i /= 10;
+                    if (i % 2 != 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        
+    }
+}
