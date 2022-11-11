c46c330eea9e6053d1f6008e1478bc5e2032eeb7
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-11 22:53:58 +0200

    Update Program.cs

--- 04. Password Validator/Program.cs
+++ 04. Password Validator/Program.cs
@@ -45,5 +45,64 @@ namespace _04._Password_Validator
             }
 
         }
+  =====================================================================================================================      
+        //    string password = Console.ReadLine();
+        //    bool charCount = true;
+        //    bool letAndDig = true;
+        //    bool countDig = true;
+        //    ValidationCharactersCount(password, ref charCount);
+        //    ValidationLettersAndDigits(password, ref letAndDig);
+        //    ValidationCountDigits(password, ref countDig);
+
+        //    if (charCount)
+        //    {
+        //        if (letAndDig)
+        //        {
+        //            if (countDig)
+        //            {
+        //                Console.WriteLine("Password is valid");
+        //            }
+        //        }
+        //    }
+        //}
+
+        //static void ValidationCharactersCount(string password, ref bool charCount)
+        //{
+        //    if (password.Length < 6 || password.Length > 10)
+        //    {
+        //        Console.WriteLine("Password must be between 6 and 10 characters");
+        //        charCount = false;
+        //    }
+        //}
+
+        //static void ValidationLettersAndDigits(string password, ref bool letAndDig)
+        //{
+        //    for (int i = 0; i < password.Length; i++)
+        //    {
+        //        if (!char.IsDigit(password[i]) && !char.IsLetter(password[i]))
+        //        {
+        //            Console.WriteLine("Password must consist only of letters and digits");
+        //            letAndDig = false;
+        //            break;
+        //        }
+        //    }
+        //}
+
+        //static void ValidationCountDigits(string password, ref bool countDig)
+        //{
+        //    int counterDigits = 0;
+        //    foreach (var ch in password)
+        //    {
+        //        if (char.IsDigit(ch))
+        //        {
+        //            counterDigits++;
+        //        }
+        //    }
+        //    if (counterDigits < 2)
+        //    {
+        //        Console.WriteLine("Password must have at least 2 digits");
+        //        countDig = false;
+        //    }
+        //}
     }
 }
