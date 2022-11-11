837f0437af52457f4c8169c7151b016379be0532
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-11 23:39:31 +0200

    Update Program.cs

--- 06. Middle Characters/Program.cs
+++ 06. Middle Characters/Program.cs
@@ -21,5 +21,39 @@ namespace _06._Middle_Characters
                 Console.WriteLine(input[input.Length / 2].ToString());
             }
         }
+        ================================================================================================
+        //static void Main(string[] args)
+        //{
+        //    string input = Console.ReadLine();
+        //    PrintMidCharOfInput(input);
+        //}
+
+        //static void PrintMidCharOfInput(string input) 
+        //{
+        //    if (input.Length % 2 == 0)
+        //    {
+        //        GetMidCharIfLenghtEven(input);
+        //    }
+        //    else
+        //    {
+        //        GetMidCharIfLenghtNotEven(input);
+        //    }
+        //}
+
+        //static void GetMidCharIfLenghtEven(string input)
+        //{
+        //    string midTwoChars = string.Empty;
+        //    for (int i = (input.Length / 2) - 1; i <= input.Length / 2; i++)
+        //    {
+        //        midTwoChars += input[i];
+        //    }
+        //    Console.WriteLine(midTwoChars);
+        //}
+
+        //static void GetMidCharIfLenghtNotEven(string input)
+        //{
+        //    char midChar = input[(char)(input.Length / 2)];
+        //    Console.WriteLine(midChar);
+        //}
     }
 }
