9ff43d4c8980c41e1978975ba89cd86c7e7c63c2
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-12 16:11:16 +0200

    Update Program.cs

--- 10. Top Number/Program.cs
+++ 10. Top Number/Program.cs
@@ -146,9 +146,8 @@ namespace _10._Top_Number
 //            int sum = 0;
 //            while (n != 0)
 //            {
-//                int lastDigit = n % 10;
+//                sum += n % 10;
 //                n = n / 10;
-//                sum += lastDigit;
 //            }
 //            return sum;
 //        }
