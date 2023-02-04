1948eda092630a012d447e54201c66186828fe33
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-04 23:29:23 +0200

    Update Program.cs

--- 06. Middle Characters/Program.cs
+++ 06. Middle Characters/Program.cs
@@ -1,59 +1,24 @@
-﻿using System;
+string input = Console.ReadLine();
 
-namespace _06._Middle_Characters
+if (input.Length % 2 == 0)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string input = Console.ReadLine();
-            PrintMidlChar(input);
-        }
-
-        static void PrintMidlChar(string input)
-        {
-            if (input.Length % 2 == 0)
-            {
-                Console.WriteLine(input[input.Length / 2 - 1].ToString() + input[input.Length / 2].ToString());
-            }
-            else
-            {
-                Console.WriteLine(input[input.Length / 2].ToString());
-            }
-        }
-        ================================================================================================
-        //static void Main(string[] args)
-        //{
-        //    string input = Console.ReadLine();
-        //    PrintMidCharOfInput(input);
-        //}
-
-        //static void PrintMidCharOfInput(string input) 
-        //{
-        //    if (input.Length % 2 == 0)
-        //    {
-        //        GetMidCharIfLenghtEven(input);
-        //    }
-        //    else
-        //    {
-        //        GetMidCharIfLenghtNotEven(input);
-        //    }
-        //}
-
-        //static void GetMidCharIfLenghtEven(string input)
-        //{
-        //    string midTwoChars = string.Empty;
-        //    for (int i = (input.Length / 2) - 1; i <= input.Length / 2; i++)
-        //    {
-        //        midTwoChars += input[i];
-        //    }
-        //    Console.WriteLine(midTwoChars);
-        //}
-
-        //static void GetMidCharIfLenghtNotEven(string input)
-        //{
-        //    char midChar = input[(char)(input.Length / 2)];
-        //    Console.WriteLine(midChar);
-        //}
-    }
+    PrintMiddleChar(input);
 }
+else
+{
+    PrintMiddleChars(input);
+}
+
+
+
+void PrintMiddleChar(string input)
+{
+    Console.Write(input[input.Length / 2 - 1]);
+    Console.Write(input[input.Length / 2]);
+}
+
+void PrintMiddleChars(string input)
+{
+    Console.WriteLine(input[input.Length / 2]);
+}
+
