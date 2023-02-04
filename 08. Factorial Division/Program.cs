95641d0d700c7003317740a60478d3e777ea749c
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-05 00:42:41 +0200

    Update Program.cs

--- 08. Factorial Division/Program.cs
+++ 08. Factorial Division/Program.cs
@@ -1,36 +1,28 @@
-using System;
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
 
-namespace _08.Factorial_Division
+Console.WriteLine($"{(GetFactorialN1(n1) / GetFactorialN2(n2)):f2}");
+
+
+
+decimal GetFactorialN1(int n1)
 {
-    class Program
+    decimal factorialN1 = 1;
+    while (n1 > 0)
     {
-        static void Main(string[] args)
-        {
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
-         
-            Console.WriteLine($"{PrintFactorialN1(n1) / PrintFactorialN2(n2):f2}");
-        }
-
-        static decimal PrintFactorialN1(int n1) 
-        {
-            decimal sum1 = 1;
-            for (int i = 2; i <= n1; i++)
-            {
-                sum1 *= i;
-            }
-            return sum1; 
-        }
-
-        static decimal PrintFactorialN2(int n2)
-        {
-            decimal sum2 = 1;
-            for (int i = 2; i <= n2; i++)
-            {
-                sum2 *= i;
-            }
-            return sum2;
-        }
-
+        factorialN1 *= n1;
+        n1--;
     }
+    return factorialN1;
+}
+
+decimal GetFactorialN2(int n2)
+{
+    decimal factorialN2 = 1;
+    while (n2 > 0)
+    {
+        factorialN2 *= n2;
+        n2--;
+    }
+    return factorialN2;
 }
