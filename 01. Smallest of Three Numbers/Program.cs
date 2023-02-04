d9947a8953c439f121746879b6e2c999be498b2b
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-04 19:37:55 +0200

    Update Program.cs

--- 01. Smallest of Three Numbers/Program.cs
+++ 01. Smallest of Three Numbers/Program.cs
@@ -1,83 +1,87 @@
-using System;
-using System.Linq;
-
 namespace _01._Smallest_of_Three_Numbers
 {
-    class Program
+    internal class Program
     {
-
         static void Main(string[] args)
         {
             int n1 = int.Parse(Console.ReadLine());
             int n2 = int.Parse(Console.ReadLine());
             int n3 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(GetMinValue(n1, n2, n3));
+            PrintTheSmallestOne(n1, n2, n3);
         }
 
-        static int GetMinValue(int n1, int n2, int n3)
+        static void PrintTheSmallestOne(int n1, int n2, int n3)
         {
-            int minValue = n1;
-            if (n2 < minValue)
+            if (n1 <= n2 && n1 <= n3)
             {
-                minValue = n2;
+                Console.WriteLine(n1);
             }
-            if (n3 < minValue)
+            else if (n2 <= n1 && n2 <= n3)
             {
-                minValue = n3;
+                Console.WriteLine(n2);
             }
-            return minValue;
+            else
+            {
+                Console.WriteLine(n3);
+            }
         }
-------------------------------------------------------------------------------------------
-        //static void Main(string[] args)
-        //{
-        //    int num1 = int.Parse(Console.ReadLine());
-        //    int num2 = int.Parse(Console.ReadLine());
-        //    int num3 = int.Parse(Console.ReadLine());
-
-        //    Console.WriteLine(GetMinValue(num1, num2, num3));
-        //}
-
-        //static int GetMinValue(int num1, int num2, int num3)
-        //{
-        //    int[] allNums = { num1, num2, num3 };
-        //    int minValue = allNums[0];
-        //    foreach (var num in allNums)
-        //    {
-        //        if (num < minValue)
-        //        {
-        //            minValue = num;
-        //        }
-        //    }
-        //    return minValue;
-        //}
--------------------------------------------------------------------------------------------------
-
-        //static void Main(string[] args)
-        //{
-        //    int[] arr = new int[3];
-        //    for (int i = 0; i < 3; i++)
-        //    {
-        //        int n = int.Parse(Console.ReadLine());
-        //        arr[i] = n;
-        //    }
-        //    GetSmolestNum(arr);
-        //}
-
-        //static void GetSmolestNum(int[] arr)
-        //{
-        //    int smolestNum = int.MaxValue;
-        //    for (int i = 0; i < 3; i++)
-        //    {
-        //        if (arr[i] < smolestNum)
-        //        {
-        //            smolestNum = arr[i];
-        //        }
-        //    }
-        //    Console.WriteLine(smolestNum);
-        //}
-
-
-
     }
 }
+-----------------------------------------------------
+
+//namespace _01._Smallest_of_Three_Numbers
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n1 = int.Parse(Console.ReadLine());
+//            int n2 = int.Parse(Console.ReadLine());
+//            int n3 = int.Parse(Console.ReadLine());
+
+//            PrintTheSmallestOne(n1, n2, n3);
+//        }
+
+//        static void PrintTheSmallestOne(int n1, int n2, int n3)
+//        {
+//            int result = Math.Min(n1, Math.Min(n2, n3));
+//            Console.WriteLine(result);
+//        }
+//    }
+//}
+-----------------------------------------------------
+
+//namespace _01._Smallest_of_Three_Numbers
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = new int[3];
+
+//            for (int i = 0; i < 3; i++)
+//            {
+//                arr[i] = int.Parse(Console.ReadLine());
+//            }
+
+//            PrintTheSmallestOne(arr);
+//        }
+
+//        static void PrintTheSmallestOne(int[] arr)
+//        {
+//            int minValue = int.MaxValue;
+
+//            foreach (var num in arr)
+//            {
+//                if (num < minValue)
+//                {
+//                    minValue = num;
+//                }
+//            }
+
+//            Console.WriteLine(minValue);
+//        }
+//    }
+//}
+
