4533f984bb6186906c68383a671072579fea42dc
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-04 21:21:16 +0200

    Update Program.cs

--- 03. Characters in Range/Program.cs
+++ 03. Characters in Range/Program.cs
@@ -52,3 +52,31 @@ namespace _11._Array_Manipulator
 
     }
 }
+========================================================================================================
+
+//char ch1 = char.Parse(Console.ReadLine());
+//char ch2 = char.Parse(Console.ReadLine());
+
+
+//GetSwapInputChars(ref ch1, ref ch2);
+//PrintCharactesBetwin(ch1, ch2);
+
+
+//void GetSwapInputChars(ref char ch1, ref char ch2)
+//{
+//    if (ch1 > ch2)
+//    {
+//        char temp = ch2;
+//        ch2 = ch1;
+//        ch1 = temp;
+//    }
+//}
+
+
+//void PrintCharactesBetwin(char ch1, char ch2)
+//{
+//    for (int i = ch1 + 1; i < ch2; i++)
+//    {
+//        Console.Write("{0} ", (char)i);
+//    }
+//}
