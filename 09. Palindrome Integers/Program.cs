97774522c83509352e46e77287b78cf797dfa839
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-05 14:08:10 +0200

    Update Program.cs

--- 09. Palindrome Integers/Program.cs
+++ 09. Palindrome Integers/Program.cs
@@ -1,33 +1,54 @@
-﻿using System;
-
-namespace _09._Palindrome_Integers
+while (true)
 {
-    class Program
+    string number = Console.ReadLine();
+
+    if (number == "END")
     {
-        static void Main(string[] args)
-        {
-            string n;
-            while ((n = Console.ReadLine()) != "END")
-            {
-                Console.WriteLine(PrintTrueOrFalse(n));
-            }
-        }
-
-        static bool PrintTrueOrFalse(string n)
-        {
-            string reversedNum = string.Empty;
-            bool result = false;
-
-            for (int i = n.Length - 1; i >= 0; i--)
-            {
-                reversedNum += n[i];
-            }
-            if (n == reversedNum)
-            {
-                result = true;
-            }
-
-            return result;
-        }
+        break;
     }
+
+    Console.WriteLine(GetReversedNumAndPrint(number).ToString().ToLower());
 }
+
+
+bool GetReversedNumAndPrint(string number)
+{
+    string revNumber = string.Empty;
+
+    foreach (var item in number.Reverse())
+    {
+        revNumber += item;
+    }
+
+    if (revNumber == number)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+
+
+//string n;
+//while ((n = Console.ReadLine()) != "END")
+//{
+//    Console.WriteLine(PrintTrueOrFalse(n));
+//}
+
+//static bool PrintTrueOrFalse(string n)
+//{
+//    string reversedNum = string.Empty;
+//    bool result = false;
+
+//    for (int i = n.Length - 1; i >= 0; i--)
+//    {
+//        reversedNum += n[i];
+//    }
+//    if (n == reversedNum)
+//    {
+//        result = true;
+//    }
+
+//    return result;
+//}
